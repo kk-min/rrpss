1c3e61782193fb88010e634a03a999d956c74d8e
MINK <76023265+mink0003@users.noreply.github.com>
2021-10-20 11:14:17 +0800

    Initial draft for time system using a clock class

---
+++ Classes/Clock.java
@@ -0,0 +1,35 @@
+public class Clock {
+    private static int clockValue = 0; // Tracked in seconds
+
+    public Clock(){}
+
+    public static void advanceTime(int period, String interval){
+
+        switch(interval){
+            case "second":
+                clockValue += period;
+                break;
+            case "minute":
+                clockValue += 60*period;
+                break;
+            case "hour":
+                clockValue += 60*60*period;
+            break;
+            case "day":
+                clockValue += 24*60*60*period;
+                break;
+            case "month":
+                clockValue += 30*24*60*60*period; // Assume every month has 30 days
+                break;
+            case "year":
+                clockValue += 12*30*24*60*60*period;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static int getTime(){
+        return clockValue;
+    }
+}
