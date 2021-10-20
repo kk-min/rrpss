c87fa7483240a358514d9726e11f8be174b4f7aa
MINK <76023265+mink0003@users.noreply.github.com>
2021-10-20 11:12:51 +0800

    Added Header to generateReport (in progress)

--- Classes/SalesRevenueReport.java
+++ Classes/SalesRevenueReport.java
@@ -12,6 +12,26 @@ public class SalesRevenueReport {
         }
 
         this.period = period;
+
+
+
+    }
+
+    public void generateReport(){
+        String Header = "SALES REVENUE REPORT" +" ("+this.period+")";
+        int rowLength = 63;
+        //Line 1:
+        for (int i = 0; i < rowLength; i++) { // print a ----- row
+            System.out.print("-");
+        }
+        System.out.println();
+        //Line 2:
+        String leftFormat = "%-"+((rowLength/2)-(Header.length()/2))+"s";
+        String rightFormat = "%"+((rowLength/2)-(Header.length()/2))+"s";
+        System.out.format(leftFormat, "|");
+        System.out.print(Header);
+        System.out.format(rightFormat, "|");
+        System.out.println();
     }
 
 
