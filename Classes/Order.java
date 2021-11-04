dea1256825c326b61ef778458f40606bf47a5431
MINK <76023265+mink0003@users.noreply.github.com>
2021-11-04 19:51:39 +0800

    Minor name changes

--- Classes/Order.java
+++ Classes/Order.java
@@ -1,24 +1,23 @@
 public class Order{
     private int tableID;
-    private MenuItem[] itemList;
-    private Staff orderCreator;
+    private AMenuItem[] itemList;
+    private Staff createdBy;
     private final double TAX_RATE = 0.07; // 7% tax
     private final double DISCOUNT_RATE = 0.05;
-    private Customer patron;
     private double tax;
     private double subTotal; // before taxes, discounts included
     private double grandTotal; // after taxes
-    //private Time timestamp; // TO DO!
+    //private Time dateTime; // TO DO!
 
-    public Order(int tableID, MenuItem[] itemList, Staff orderCreator, boolean isMember){
+    public Order(int tableID, AMenuItem[] itemList, Staff orderCreator, boolean isMember){
         this.tableID = tableID;
-        this.orderCreator = orderCreator;
-        this.itemList = new MenuItem[itemList.length];
+        this.createdBy = orderCreator;
+        this.itemList = new AMenuItem[itemList.length];
         this.subTotal = 0;
         //this.timestamp = ?
         int i = 0;
 
-        for (MenuItem item : itemList){
+        for (AMenuItem item : itemList){
             this.itemList[i] = item; // insert all MenuItems from parameter list into our Order attribute
             this.subTotal += item.getPrice(); // add all prices of individual MenuItems into our subTotal attribute
         }
