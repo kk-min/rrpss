aca8b7c4ea00be5489c719557907b68bec7637a7
PhantomIntegration <39938498+PhantomIntegration@users.noreply.github.com>
2021-11-06 15:45:57 +0800

    Update Table.java

--- Class/Table.java
+++ Class/Table.java
@@ -1,13 +1,21 @@
-public class Table {
-	private int ID;
-	private int capacity;
-	private boolean occupied;
-	private static int tableNo = 1;
+package Project;
 
-	public Table(int capacity) {
-		this.ID = tableNo++;
-		this.capacity = capacity;
+import Project.MainApp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Table {
+	private int capacity;
+	public static int MAX_CAPACITY = 10;
+	private int ID;
+	private boolean occupied;
+
+	public Table(int tableNo, int cap) {
+		this.ID = tableNo;
 		this.occupied = false;
+		if(cap > 10) capacity = 10;
+		else capacity = cap;
 	}
 
 	public boolean isOccupied() {
@@ -25,6 +33,29 @@ public class Table {
 	public int getID() {
 		return ID;
 	}
+	
+	public int getCapacity() {
+		return capacity;
+	}
+	
+	public void setCapacity(int cap) {
+		if(cap > 10) capacity = 10;
+		else capacity = cap;
+	}
+	
+	public static Table getTableByID(int id) {
+        for (Table t : MainApp.tableCollection) {
+            if (t.getID() == id)
+                return t;
+        }
+        return null;
+	}
+	
+	public static ArrayList<Table> getTheOtherHalf(ArrayList<Table> input){
+		ArrayList<Table> output = new ArrayList<Table>(MainApp.tableCollection);
+		for(Table t: input)
+			output.remove(t);
+		output.sort(Comparator.comparingInt(o -> o.capacity));
+		return output;
+	}
 }
-
-// find table for reservation(no, resDate, session)
