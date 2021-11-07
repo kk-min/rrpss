9a0ba2cb03f6b8982a206e41fc78696e48b201e6
PhantomIntegration <39938498+PhantomIntegration@users.noreply.github.com>
2021-11-07 22:07:28 +0800

    Create UIExecuteTimer.java

---
+++ Timed/UIExecuteTimer.java
@@ -0,0 +1,18 @@
+package Project;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class UIExecuteTimer {
+
+	public static void main(String[] args) {
+	}
+	
+	public static void runScheduler() {
+		UITimerExample checkExpiredReservationTask = new UITimerExample("Task");
+
+		Timer t = new Timer();
+		t.scheduleAtFixedRate(checkExpiredReservationTask, 0, 60 * 1000);
+	}
+}
