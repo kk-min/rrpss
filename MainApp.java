7b1cb8a9720b5faae1e24d6fe0ee8f8c3ae2f24c
her <58970041+limherhuey@users.noreply.github.com>
2021-11-07 23:32:36 +0800

    created OrderInterface and populated main function

--- MainApp.java
+++ MainApp.java
@@ -1,37 +1,180 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainApp {
 	
-	public static ArrayList<Table> tableCollection;
+	// public static ArrayList<Table> tableCollection;
 	
-	public static ArrayList<Reservation> reservationCollection;
+	// public static ArrayList<Reservation> reservationCollection;
 	
-	private static int tableTrack[] = {2,2,2,2,2};
+	// private static int tableTrack[] = {2,2,2,2,2};
 	
-	public static ArrayList<Staff> staffCollection;
+	// public static ArrayList<Staff> staffCollection;
 	
-	public static ArrayList<Order> orderCollection;
+	// public static ArrayList<Order> orderCollection;
 	
-	public static ArrayList<MenuItem> menuItemCollection;
+	// public static ArrayList<MenuItem> menuItemCollection;
 	
-	public static ArrayList<MenuItem[]> menuItemSellRecord;
+	public static void main(String args[]) {
+			Scanner sc = new Scanner(System.in);
+
+			String restaurantName = "HELLOWORLD";
+			int rowLength = 63;
+			
+
+			int choice = 0, subchoice = 0;
+			while (choice <= 5) {
+				//Line 1:
+				System.out.printf("-".repeat(rowLength));
+				System.out.println();
+
+				//Line 2:
+				String leftFormat = "%-"+((rowLength/2)-(restaurantName.length()/2))+"s";
+				String rightFormat = "%"+((rowLength/2)-(restaurantName.length()/2))+"s";
+				System.out.format(leftFormat, " ");
+				System.out.print(restaurantName);
+				System.out.format(rightFormat, " ");
+				System.out.println();
+
+				//Line 3:
+				System.out.printf("-".repeat(rowLength));
+				System.out.println();
+
+				//Line 4+:
+				System.out.println("Select an option:");
+				System.out.println();
+				System.out.println("1. Menu");
+				System.out.println("2. Order");
+				System.out.println("3. Reservation");
+				System.out.println("4. Print Order Invoice");
+				System.out.println("5. Print Sale Revenue Report");
+				System.out.println("6. Exit");
+
+				//Line 11:
+				System.out.printf("-".repeat(rowLength));
+				System.out.println();
+
+				System.out.print("Enter your choice: ");
+				do {
+					choice = sc.nextInt();
+				} while (choice > 5 || choice <= 0);
 	
-	public MainApp() {
-		
-	}
-	
-	private static void createTableCollection() {
-		int i = 0,j,n = 1,capacity = 2;
-		while(capacity <= 10) {
-			for(j = 0; j < tableTrack[i]; j++) {
-				Table table = new Table(n++,capacity);
-				tableCollection.add(table);
+				switch(choice) {
+					case 1: //Menu submenu
+						while (subchoice < 8) {
+							System.out.println("Select an option:");
+							System.out.println();
+							System.out.println("1. View Menu");
+							System.out.println("2. Create Ala Carte item");
+							System.out.println("3. Update Ala Carte item");
+							System.out.println("4. Remove Ala Carte item");
+							System.out.println("5. Create Promotional item");
+							System.out.println("6. Update Promotional item");
+							System.out.println("7. Remove Promotional item");
+							System.out.println("8. Go Back");
+
+							System.out.printf("-".repeat(rowLength));
+							System.out.println();
+
+							System.out.print("Enter your choice: ");
+							do {
+								subchoice = sc.nextInt();
+							} while (subchoice > 8 || subchoice <= 0);
+
+							switch (subchoice) {
+								case 1:
+								case 2:
+								case 3:
+								case 4:
+								case 5:
+								case 6:
+								case 7:
+							}
+						}
+						break;
+					case 2:	//Order submenu
+						while (subchoice < 5) {
+							System.out.println("Select an option:");
+							System.out.println();
+							System.out.println("1. Create Order");
+							System.out.println("2. View Order");
+							System.out.println("3. Add item to Order");
+							System.out.println("4. Remove item from Order");
+							System.out.println("5. Go Back");
+						
+							System.out.printf("-".repeat(rowLength));
+							System.out.println();
+
+							System.out.print("Enter your choice: ");
+							do {
+								subchoice = sc.nextInt();
+							} while (subchoice > 5 || subchoice <= 0);
+
+							switch (subchoice) {
+								case 1:
+									OrderInterface.create(staffCollection);
+									break;
+								case 2:
+									OrderInterface.view(OngoingOrders);
+									break;
+								case 3:
+									OrderInterface.add(OngoingOrders);
+									break;
+								case 4:
+									OrderInterface.remove(OngoingOrders, OrderHistory);
+							}
+						}
+						break;
+					case 3:	// Reservation submenu
+						while (subchoice < 5) {
+							System.out.println("Select an option:");
+							System.out.println();
+							System.out.println("1. Create Reservation booking");
+							System.out.println("2. Check Reservation booking");
+							System.out.println("3. Remove Reservation booking");
+							System.out.println("4. Check Table availability");
+							System.out.println("5. Go back");
+
+							System.out.printf("-".repeat(rowLength));
+							System.out.println();
+
+							System.out.print("Enter your choice: ");
+							do {
+								subchoice = sc.nextInt();
+							} while (subchoice > 5 || subchoice <= 0);
+
+							switch (subchoice) {
+								case 1:
+								case 2:
+								case 3:
+								case 4:
+							}
+						}
+						break;
+					case 4:	// Print Order Invoice
+						// OrderInterface.checkout(OrderHistory);
+						break;
+					case 5: // Print Sale Revenue Report
+						// TODO
+						break;
+				}
 			}
-			i++;
-			capacity += 2;
-		}
+	
+			sc.close();	
 	}
+	
+	// private static void createTableCollection() {
+	// 	int i = 0,j,n = 1,capacity = 2;
+	// 	while(capacity <= 10) {
+	// 		for(j = 0; j < tableTrack[i]; j++) {
+	// 			Table table = new Table(n++,capacity);
+	// 			tableCollection.add(table);
+	// 		}
+	// 		i++;
+	// 		capacity += 2;
+	// 	}
+	// }
 }
 
 //pull new attributes as you need
---
+++ OrderInterface.java
@@ -0,0 +1,133 @@
+import java.util.Scanner;
+
+public class OrderInterface {
+
+    private static ArrayList<Order> OrderHistory;
+
+    public OrderInterface() {
+        OrderHistory = new ArrayList<Order>();
+    }
+
+    public ArrayList<Order> getOrderHistory() {
+        return OrderHistory;
+    }
+
+    public static Order create(ArrayList<Staff> staffList) {
+        System.out.print("Please enter the Table ID for the order: ");
+        tableID = in.nextInt();
+        Table.getTableById(tableID).setOccupied();
+        System.out.print("Is the customer a member? (Y/N): ");
+        isMember = in.nextChar();
+        if (isMember == 'Y') isMember = true;
+        else isMember = false;
+        for (Staff staff : staffList){
+            System.out.println(staff.getEmployeeID(), ": ", staff.getName());
+        }
+        System.out.print("Please enter your staff ID: ");
+        staffID = in.nextInt();
+        Order userOrder = new Order(tableID, staffID, isMember);
+        System.out.println("You have created a new order. Order ID: %d\n", userOrder.getID());
+        return userOrder;
+    }
+
+    public static void view() {
+        int flag = 0;
+        System.out.print("Please enter the Order ID to view: ");
+        orderID = in.nextInt();
+        for (Order viewOrder : OrderHistory){
+            if (viewOrder.getID() == orderID){
+                userOrder = viewOrder;
+                flag = 1; // We found our Order
+                break;
+            }
+        }
+        if (flag == 0){
+            System.out.println("Invalid Order ID.");
+        }
+        else{
+            Printer.printInvoice(userOrder); // We can just print the invoice from the printer class, because that's basically the summary of the order...
+        }
+    }
+
+    public static void add() {
+        int flag = 0;
+        System.out.print("Please enter the Order ID to add items to: ");
+        orderID = in.nextInt();
+        for (Order viewOrder : OrderHistory){
+            if (viewOrder.getID() == orderID){
+                userOrder = viewOrder;
+                flag = 1; // We found our Order
+                break;
+            }
+        }
+        if (flag == 0){
+            System.out.println("Invalid Order ID.");
+        }
+        else{
+            flag = 0;
+            System.out.println("You have chosen Order %d. Current items in this order:", userOrder.getID());
+            itemList = userOrder.getitemList();
+            for (int i = 0; i < itemList.length(); i++) {
+                System.out.println("%d. %s", i+1, itemList[i].getName());
+            }
+            
+            // Add Item
+            // TODO: display menu items
+            System.out.println("Please select an item to add to order: ");
+            itemID = in.nextInt();  // TODO: convert into AMenuItem
+            System.out.println("Enter the quantity: ");
+            itemQty = in.nextInt();
+            userOrder.addItem(itemID, itemQty);
+        }
+    }
+
+    public static void remove() {
+        int flag = 0;
+        System.out.print("Please enter the Order ID to remove items from: ");
+        orderID = in.nextInt();
+        for (Order viewOrder : OrderHistory){
+            if (viewOrder.getID() == orderID){
+                userOrder = viewOrder;
+                flag = 1; // We found our Order
+                break;
+            }
+        }
+        if (flag == 0){
+            System.out.println("Invalid Order ID.");
+        }
+        else{
+            System.out.println("You have chosen Order %d. Current items in this order:", userOrder.getID());
+            itemList = userOrder.getitemList();
+            for (int i = 0; i < itemList.length(); i++) {
+                System.out.println("%d. %s", i+1, itemList[i].getName());
+            }
+
+            // Remove Item
+            System.out.println("Please select the item you wish to remove: ");
+            itemID = in.nextInt();  // TODO: convert to object
+            userOrder.removeItem(itemID);
+            System.out.println("Item successfully Removed.");
+        }
+    }
+
+    public static void checkout() {
+        System.out.print("Please enter the Order to checkout: ");
+        orderID = in.nextInt();
+        for (Order viewOrder : OrderHistory){
+            if (viewOrder.getID() == orderID){
+                userOrder = viewOrder;
+                flag = 1; // We found our Order
+                break;
+            }
+        }
+        if (flag == 0) {
+            System.out.println("Invalid Order ID.");
+        }
+        else {
+            Printer.printInvoice(userOrder);
+            int tableID = userOrder.getTableID();
+            Table.getTableById(tableID).setEmpty();
+            System.out.println("Order successfully checked out.");
+        }
+    }
+}
