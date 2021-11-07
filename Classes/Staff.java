2a2afd9b507cb35614b37f6058eebb8b6a44f02b
her <58970041+limherhuey@users.noreply.github.com>
2021-11-07 16:20:36 +0800

    added getters to Staff class

--- Classes/Staff.java
+++ Classes/Staff.java
@@ -1,13 +1,29 @@
 public class Staff {
     private int employeeID;
     private String name;
-    private String gender;
+    private char gender;
     private String jobTitle;
  
-    public Staff(int ID, String name, String gender, String jobTitle) {
-        this.employeeID = ID;
+    public Staff(int employeeID, String name, char gender, String jobTitle) {
+        this.employeeID = employeeID;
         this.name = name;
         this.gender = gender;
         this.jobTitle = jobTitle;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
     }
 }
