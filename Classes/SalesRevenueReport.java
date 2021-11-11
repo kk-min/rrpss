a88f0512ac7cdf964f4de7d0c057f188dc24fb88
MINK <76023265+mink0003@users.noreply.github.com>
2021-11-11 17:47:10 +0800

    Updated orderList in SalesRevenueReport from array to ArrayList

--- Classes/SalesRevenueReport.java
+++ Classes/SalesRevenueReport.java
@@ -1,17 +1,17 @@
 public class SalesRevenueReport {
-    private Order[] orderList;
+    private ArrayList<Order> orderList;
     private Staff[] staffList;
     private String period;
     private double totalRevenue;
 
-    public SalesRevenueReport(Order[] orderList, Staff[] staffList, String period){
+    public SalesRevenueReport(ArrayList<Order> orderList, Staff[] staffList, String period){
         int i = 0;
         this.totalRevenue = 0;
-        this.orderList = new Order[orderList.length];
+        this.orderList = new ArrayList<Order>;
         for (Order order: orderList){
-            this.orderList[i] = order;
+            this.orderList[i].add(order);
             this.totalRevenue += order.getGrandTotal();
-            i++;
+
         }
 
         i = 0;
