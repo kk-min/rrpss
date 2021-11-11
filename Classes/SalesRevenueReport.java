ec6160959a496c4e62fcd40ff47826e2ed9e15b3
MINK <76023265+mink0003@users.noreply.github.com>
2021-11-11 18:11:00 +0800

    Edited SalesReport to pick out orders from OrderHistory based on time period

--- Classes/SalesRevenueReport.java
+++ Classes/SalesRevenueReport.java
@@ -9,18 +9,34 @@ public class SalesRevenueReport {
         this.totalRevenue = 0;
         this.orderList = new ArrayList<Order>;
         this.period = period;
-        LocalDate currentDate = DateTimeFormatHelper.getTodayDate(false);
+        String currentDate = DateTimeFormatHelper.formatToStringDate(DateTimeFormatHelper.getTodayDate(false));
         switch(this.period){
             case "DAY":
-                for ()
+                for (Order order : orderList){
+                    if ((order.getdateTime()[0]+order.getdateTime[1]) == (currentDate[0]+currentDate[1]))
+                    {
+                        orderList.add(order);
+                        this.totalRevenue += order.getGrandTotal();
+                    }
+                }
             case "MONTH":
+                for (Order order : orderList){
+                    if ((order.getdateTime()[3]+order.getdateTime[4]) == (currentDate[3]+currentDate[4]))
+                    {
+                        orderList.add(order);
+                        this.totalRevenue += order.getGrandTotal();
+                    }
+                }
             case "YEAR":
+                for (Order order : orderList){
+                    if ((order.getdateTime()[7]+order.getdateTime[1]) == (currentDate[0]+currentDate[8]))
+                    {
+                        orderList.add(order);
+                        this.totalRevenue += order.getGrandTotal();
+                    }
+                }
         }
-        for (Order order: orderList){
-            this.orderList[i].add(order);
             this.totalRevenue += order.getGrandTotal();
-
-        }
 
         i = 0;
         this.staffList = new Staff[staffList.length];
