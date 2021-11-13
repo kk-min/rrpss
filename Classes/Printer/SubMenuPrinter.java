f22b057a454efc170d2c2c36be3ed0c74ddf8452
MINK <76023265+mink0003@users.noreply.github.com>
2021-11-13 12:01:37 +0800

    Added function for viewing Order Summary

--- Classes/Printer/SubMenuPrinter.java
+++ Classes/Printer/SubMenuPrinter.java
@@ -3,8 +3,11 @@ package Classes.Printer;
 import java.util.Scanner;
 
 import Classes.AMenuItem.MenuManager;
+import Classes.AMenuItem.AMenuItem;
 import Classes.Order.OrderManager;
+import Classes.Order.Order;
 import Classes.Reservation.ReservationManager;
+import java.util.Map;
 
 public class SubMenuPrinter {
 
@@ -90,6 +93,88 @@ public class SubMenuPrinter {
                     return -1;
             }
         }
+    }
+
+    public static void printOrderSummary(Order viewOrder){
+        int rowLength = 63;
+
+        //Line 1:
+        System.out.printf("-".repeat(rowLength));
+        System.out.println();
+
+        //Line 2:
+        String orderSummary = "ORDER SUMMARY";
+        String leftFormat = "%-"+((rowLength/2)-(orderSummary.length()/2))+"s";
+        String rightFormat = "%"+((rowLength/2)-(orderSummary.length()/2))+"s";
+        System.out.format(leftFormat, " ");
+        System.out.print(orderSummary);
+        System.out.format(rightFormat, " ");
+        System.out.println();
+
+        //Line 3:
+        System.out.printf("-".repeat(rowLength));
+        System.out.println();
+
+        //Line 4:
+        String OrderNo = "Order no. "+viewOrder.getID();
+        System.out.println(OrderNo);
+
+        //Line 5:
+        String tableFormat = String.format("%"+(OrderNo.length()-8)+"s");
+        System.out.print("Table ID");
+        System.out.format(tableFormat, viewOrder.getTableID());
+
+        //Line 6:
+        System.out.printf("-".repeat(rowLength));
+        System.out.println();
+
+        //Items:
+        for (var entry : viewOrder.getItemList().entrySet()){
+            AMenuItem item = entry.getKey();
+            String itemName = item.getName();
+            String leftString = itemName+" x"+entry.getValue();
+            System.out.print(leftString);
+            String priceFormat = "%"+(rowLength-leftString.length())+"s";
+            String priceString = "      $"+String.format("%.2f",item.getPrice()*entry.getValue())+"      ";
+            System.out.format(priceFormat, priceString);
+            System.out.println();
+        }
+        System.out.printf("-".repeat(rowLength));
+        System.out.println();
+
+        double subTotal = viewOrder.getSubTotal();
+        double grandTotal = viewOrder.getGrandTotal();
+        //Bottom segment:
+        String subTotalPrice = "$"+String.format("%.2f",subTotal);
+        String totalPrice = "$"+String.format("%.2f",grandTotal);
+        String discountRate = Integer.toString((int)(100*viewOrder.getDISCOUNT_RATE())) + "%";
+        String taxRate = Integer.toString((int)(100*viewOrder.getTAX_RATE())) + "%";
+
+        String discountFormat = String.format("%"+totalPrice.length()+"s", discountRate);
+        String subTotalFormat = String.format("%"+totalPrice.length()+"s", subTotalPrice);
+        String totalFormat = String.format("%"+totalPrice.length()+"s", totalPrice);
+        String taxFormat = String.format("%"+totalPrice.length()+"s", taxRate);
+
+        String subTotalString = "subTotal: "+subTotalFormat+"     ";
+        String discountString = "Discount: "+discountFormat+"     ";
+        String taxString = "Tax:      "+taxFormat+"     ";
+        String totalString = "Total:    "+totalFormat+"     ";
+
+        String resultFormatter = "%"+(rowLength)+"s";
+
+        System.out.format(resultFormatter, discountString);
+        System.out.println();
+        System.out.format(resultFormatter, subTotalString);
+        System.out.println();
+        System.out.format(resultFormatter, taxString);
+        System.out.println();
+        System.out.format(resultFormatter, totalString);
+        System.out.println();
+
+        System.out.printf("-".repeat(rowLength));
+        System.out.println();
+
+
     }
 
     public static int printReservationSubMenu() {
