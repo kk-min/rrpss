70cef28132074bc3ea1cebb9bc621ec7c34ce46e
her <58970041+limherhuey@users.noreply.github.com>
2021-11-13 15:30:06 +0800

    added UIPrinter

---
+++ Classes/Printer/UserInterfacePrinter.java
@@ -0,0 +1,7 @@
+package Classes.Printer;
+
+import java.util.Scanner;
+
+public abstract class UserInterfacePrinter implements Printer {
+    private static Scanner input = new Scanner(System.in);
+}
