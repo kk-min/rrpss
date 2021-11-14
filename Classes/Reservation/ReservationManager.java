ddc40bac5e94612797b058efca40776a1ac25f67
her <58970041+limherhuey@users.noreply.github.com>
2021-11-14 10:27:14 +0800

    removed unused import

--- Classes/Reservation/ReservationManager.java
+++ Classes/Reservation/ReservationManager.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 
 import java.time.format.DateTimeParseException;
